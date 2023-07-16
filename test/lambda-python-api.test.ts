ffd24f8893b6bbd5c8d992bfe47316567e355e4c
dhirajkhodade <dhirajkhodade@gmail.com>
2023-07-16 17:58:58 +0530

    Initial commit

---
+++ test/lambda-python-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaPythonApi from '../lib/lambda-python-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-python-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaPythonApi.LambdaPythonApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
